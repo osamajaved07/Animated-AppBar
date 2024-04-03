44f89d140fae5c796144b99328795f4b178af61d
Osama Javed <www.om173116@gmail.com>
2024-04-03 15:46:12 +0500

    Commit

--- lib/app_bar.dart
+++ lib/app_bar.dart
@@ -5,20 +5,20 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:insta_image_viewer/insta_image_viewer.dart';
 
 class SliverExample extends StatelessWidget {
-   SliverExample({super.key});
+  SliverExample({super.key});
   List<String> imagePaths = [
-  "images/c1.jpg",
-  "images/c2.jpg",
-  "images/c3.jpg",
+    "images/c1.jpg",
+    "images/c2.jpg",
+    "images/c3.jpg",
     "images/c4.jpg",
-  "images/c5.jpg",
-  "images/c6.jpg",
-  "images/c7.jpg",
-  "images/c8.jpg",
-  "images/c9.jpg",
-  "images/c10.jpg",
-  // Add other image paths here
-];
+    "images/c5.jpg",
+    "images/c6.jpg",
+    "images/c7.jpg",
+    "images/c8.jpg",
+    "images/c9.jpg",
+    "images/c10.jpg",
+    // Add other image paths here
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +47,7 @@ class SliverExample extends StatelessWidget {
             expandedHeight: 400,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
-                "images/coffee.jpg",
+                "images/laminate.jpg",
                 fit: BoxFit.cover,
               ),
               title: Text(
@@ -65,7 +65,7 @@ class SliverExample extends StatelessWidget {
           //     crossAxisCount: 2,
           //     itemBuilder: (context, index) {
           //       double ht = ((index % 3) + 1) * 150;
-                 
+
           //       return Padding(
           //         padding: EdgeInsets.all(10),
           //         child: ClipRRect(
@@ -83,35 +83,37 @@ class SliverExample extends StatelessWidget {
           //   ),
           // )
           SliverToBoxAdapter(
-  child: MasonryGridView.count(
-    padding: EdgeInsets.all(0),
-    shrinkWrap: true,
-    itemCount: 10, // Assuming you want to display 10 images
-    physics: NeverScrollableScrollPhysics(),
-    crossAxisCount: 2,
-    itemBuilder: (context, index) {
-      double ht = ((index % 3) + 1) * 150;
-      // Calculate the index of the image path to use based on the current index
-      int imagePathIndex = index ~/ 2; // Each pair of images will use the same path
-      if (imagePathIndex >= imagePaths.length) {
-        imagePathIndex = imagePaths.length - 1; // Ensure we don't exceed the bounds of the list
-      }
-      return Padding(
-        padding: EdgeInsets.all(10),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: InstaImageViewer(
-            child: Image.asset(
-              imagePaths[imagePathIndex],
-              fit: BoxFit.cover,
-              height: ht,
+            child: MasonryGridView.count(
+              padding: EdgeInsets.all(0),
+              shrinkWrap: true,
+              itemCount: 10, // Assuming you want to display 10 images
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              itemBuilder: (context, index) {
+                double ht = ((index % 3) + 1) * 150;
+                // Calculate the index of the image path to use based on the current index
+                int imagePathIndex =
+                    index ~/ 2; // Each pair of images will use the same path
+                if (imagePathIndex >= imagePaths.length) {
+                  imagePathIndex = imagePaths.length -
+                      1; // Ensure we don't exceed the bounds of the list
+                }
+                return Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: InstaImageViewer(
+                      child: Image.asset(
+                        imagePaths[imagePathIndex],
+                        fit: BoxFit.cover,
+                        height: ht,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        ),
-      );
-    },
-  ),
-)
+          )
         ],
       ),
     );
